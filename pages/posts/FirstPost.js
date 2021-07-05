cc73ab777da293dd159a790031c1c700ecc09f0f
Joe O'Brien <joedigitalmedia@gmail.com>
2021-07-05 16:59:49 +0100

    first commit

---
+++ pages/posts/FirstPost.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import Link from "next/link";
+
+function FirstPost() {
+  return (
+    <div>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+    </div>
+  );
+}
+
+export default FirstPost;
